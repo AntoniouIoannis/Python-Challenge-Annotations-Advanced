893fab828c64618734a41e3cd912eef6ce5513c5
AntoniouIoannis <antoniouioannis10@gmail.com>
2024-12-13 09:33:57 +0200

    name change def

--- myAdvancedAnnotation.py
+++ myAdvancedAnnotation.py
@@ -1,4 +1,4 @@
-def races(name: str, location: str, country: str, separator: str = ", ", end: str = ".\t") -> None:
+def car_racing_tracks(name: str, location: str, country: str, separator: str = ", ", end: str = ".\t") -> None:
     """
     _briefcase by i-antoniou_,
     Short Description:
@@ -11,32 +11,32 @@ def races(name: str, location: str, country: str, separator: str = ", ", end: st
         separator (str, optional): A symbol to separate the fields. Default is ", ".
         end (str, optional): A symbol to end the printed line. Default is ".\t".
     """
-    print(f"Race track: {name}{separator}Location: {location}{separator}Country: {country}{end}")
+    print(f"Car Racing Tracks: {name}{separator}Location: {location}{separator}Country: {country}{end}")
 
 
 def main() -> None:
     """Demonstrates the usage of the races function."""
     # Εισαγωγή Πίστες Formula1 με όνομα, τοποθεσία και χώρα.
     print("\n\nΠίστες Formula1 με όνομα, τοποθεσία και χώρα\n\t--------------------")
-    races("Monza", "Lombardy", "Ιταλία")
-    races("Silverstone", "Northamptonshire", "Ηνωμένο Βασίλειο")
-    races("Suzuka", "Mie Prefecture", "Ιαπωνία")
-    races("Interlagos", "Sao Paulo", "Βραζιλία", separator=" | ", end=".")
-    races("Marina Bay Street Circuit", "Singapore", "Σιγκαπούρη", separator=" | ", end=".")
-    races("Circuit de Barcelona-Catalunya", "Montmelo", "Ισπανία", separator=" | ", end=".")
-    races("Shanghai International Circuit", "Shanghai", "Κίνα",  separator=" | ", end=".")
-    races("Zandvoort", "Zandvoort", "Ολλανδία", separator=" | ", end=".")
-    races("Hungaroring", "Mogyoród", "Ουγγαρία", separator=" | ", end=".")    
-    races("Circuit de Monaco", "Monte Carlo", "Μονακό", separator=" | ", end=".")
-    races("Spa-Francorchamps", "Stavelot", "Βέλγιο", separator=" | ", end=".")  
-    races("Bahrain International Circuit", "Sakhir", "Μπαχρέιν", separator=" | ", end=".")
-    races("Albert Park Circuit", "Melbourne", "Αυστραλία", separator=" | ", end=".")
-    races("Hockenheimring", "Hockenheim", "Γερμανία", separator=" | ", end=".")
-    races("Yas Marina Circuit", "Abu Dhabi", "Ηνωμένα Αραβικά Εμιράτα", separator=" | ", end=".")
-    races("Circuit of the Americas", "Austin", "Ηνωμένες Πολιτείες της Αμερικής", separator=" | ", end=".")
-    races("Kyalami", "Midrand", "Νότια Αφρική", separator=" | ", end=".")
-    races("Circuit Paul Ricard", "Le Castellet", "Γαλλία", separator=" | ", end=".")
-    races("Istanbul Park", "Tuzla", "Τουρκία", separator=" | ", end=".\n\t--------------------\n")
+    car_racing_tracks("Monza", "Lombardy", "Ιταλία")
+    car_racing_tracks("Silverstone", "Northamptonshire", "Ηνωμένο Βασίλειο")
+    car_racing_tracks("Suzuka", "Mie Prefecture", "Ιαπωνία")
+    car_racing_tracks("Interlagos", "Sao Paulo", "Βραζιλία", separator=" | ", end=".")
+    car_racing_tracks("Marina Bay Street Circuit", "Singapore", "Σιγκαπούρη", separator=" | ", end=".")
+    car_racing_tracks("Circuit de Barcelona-Catalunya", "Montmelo", "Ισπανία", separator=" | ", end=".")
+    car_racing_tracks("Shanghai International Circuit", "Shanghai", "Κίνα",  separator=" | ", end=".")
+    car_racing_tracks("Zandvoort", "Zandvoort", "Ολλανδία", separator=" | ", end=".")
+    car_racing_tracks("Hungaroring", "Mogyoród", "Ουγγαρία", separator=" | ", end=".")    
+    car_racing_tracks("Circuit de Monaco", "Monte Carlo", "Μονακό", separator=" | ", end=".")
+    car_racing_tracks("Spa-Francorchamps", "Stavelot", "Βέλγιο", separator=" | ", end=".")  
+    car_racing_tracks("Bahrain International Circuit", "Sakhir", "Μπαχρέιν", separator=" | ", end=".")
+    car_racing_tracks("Albert Park Circuit", "Melbourne", "Αυστραλία", separator=" | ", end=".")
+    car_racing_tracks("Hockenheimring", "Hockenheim", "Γερμανία", separator=" | ", end=".")
+    car_racing_tracks("Yas Marina Circuit", "Abu Dhabi", "Ηνωμένα Αραβικά Εμιράτα", separator=" | ", end=".")
+    car_racing_tracks("Circuit of the Americas", "Austin", "Ηνωμένες Πολιτείες της Αμερικής", separator=" | ", end=".")
+    car_racing_tracks("Kyalami", "Midrand", "Νότια Αφρική", separator=" | ", end=".")
+    car_racing_tracks("Circuit Paul Ricard", "Le Castellet", "Γαλλία", separator=" | ", end=".")
+    car_racing_tracks("Istanbul Park", "Tuzla", "Τουρκία", separator=" | ", end=".\n\t--------------------\n")
 
 
 if __name__ == "__main__":
